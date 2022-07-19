78dcc6b2a6a919dee1731fc2a0402c7f5b559eca
SADYSCASTROC <sadyscastro@gmail.com>
2022-07-19 00:20:34 -0500

    validacion al campo solo numeros

--- src/index.js
+++ src/index.js
@@ -1,4 +1,31 @@
 import validator from './validator.js';
 
-console.log(validator);
 
+let num = document.getElementById('numIngresado'); 
+let numIngresadoGlobal;
+num.addEventListener("keyup", function(){
+  
+      document.getElementById("showNumber").innerHTML=this.value
+      numIngresadoGlobal=this.value
+});
+
+
+
+var boton = document.getElementById("botonValidation");
+boton.onclick = function() {
+
+      if(validator.validateNum(numIngresadoGlobal)){
+             algoritmoSadys()
+      }else{
+            alert("tiene un error")
+      }
+}
+
+
+function algoritmoSadys(){
+     alert("excelente")
+}
+   
+
+
+   
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,16 @@
 const validator = {
-  // ...
+ 
+
+     validateNum(num){
+        booleano = false
+        if(num>=0){
+            booleano= false
+        }else{
+            booleano= true
+        }
+        return booleano
+    }
+
 };
 
 export default validator;
